6da8e74874812556d60a63e51b822bcac7091e38
Michelle Hoelzer <hoelzermm@gmail.com>
2019-04-24 16:36:01 -0400

    all endpoints done

--- app.py
+++ app.py
@@ -1,30 +1,10 @@
-from flask import Flask, request
+from flask import Flask
 from flask_restful import Resource, Api, reqparse, abort
 
 import datetime
 
 app = Flask(__name__)
 api = Api(app)
-
-
-# todo_practice_list = {
-#     "todo1": {
-#         "title": "test a",
-#         "creation_date": "12/31/2010",
-#         "last_update": "1/1/2011",
-#         "due_date": "1/1/2011",
-#         "completed": True,
-#         "completed_on": "1/1/2011"
-#     },
-#     "todo2": {
-#         "title": "teszt2",
-#         "creation_date": "1/2/2013",
-#         "last_update": "1/3/2013",
-#         "due_date": "1/4/2013",
-#         "completed": False,
-#         "completed_on": ""
-#     }
-# }
 
 todo_list = {}
 
@@ -37,20 +17,18 @@ todo_id = 0
 current_datetime = datetime.datetime.now()
 
 
-def abort_if_todo_doesnt_exist(todo_id):
+def nonexistant_todo(todo_id):
     if todo_id not in todo_list:
         abort(404, message=f"{todo_id} doesn't exist")
 
 
 class TodoList(Resource):
     def get(self):
-        if not todo_list:
-            return "WTF is this? Add some friggin' todos"
         return todo_list
 
     def post(self):
         global todo_id
-        todo_id += 1
+        todo_id = int(todo_id) + 1
         todo_id = str(todo_id)
         args = parser.parse_args()
         todo_list[todo_id] = {
@@ -66,7 +44,7 @@ class TodoList(Resource):
 
 class TodoItem(Resource):
     def get(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
+        nonexistant_todo(todo_id)
         return todo_list[todo_id]
 
     def put(self, todo_id):
@@ -85,7 +63,7 @@ class TodoItem(Resource):
         return 201
 
     def delete(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
+        nonexistant_todo(todo_id)
         del todo_list[todo_id]
         return 204
 
@@ -95,5 +73,3 @@ api.add_resource(TodoItem, "/todos/<todo_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-# generic todo class; check local dic to see if another num is there, figure out what num you can assign to it; if asks for an id, get only works with current ids
